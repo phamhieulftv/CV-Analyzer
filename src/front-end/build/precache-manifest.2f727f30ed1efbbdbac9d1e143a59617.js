625e27410219c8d8bbca901106eae0e35b2bf601
Pham Ly Thanh Hieu <hieuplt@lifull-tech.vn>
2020-05-18 14:53:08 +0700

    init

---
+++ src/front-end/build/precache-manifest.2f727f30ed1efbbdbac9d1e143a59617.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d03378998dd1aae22cd1c961edec4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46dd4ccf0b8a572d4a13",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d1ace71e54405aa24146",
+    "url": "/static/js/2.a0590fe6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a0590fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46dd4ccf0b8a572d4a13",
+    "url": "/static/js/main.9300035e.chunk.js"
+  },
+  {
+    "revision": "f26ba8da7c41e270b6e8",
+    "url": "/static/js/runtime-main.c4f1d106.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
